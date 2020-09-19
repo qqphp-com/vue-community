672609dd171ce0854c8708c33bab1db86b877228
leiyong <1549684884@qq.com>
2020-09-19 22:46:15 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
-git push -f git@github.com:qqphp-com/qqphp-com.github.io.git master
+git push -f git@github.com:qqphp-com/vue-community.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+#git push -f git@github.com:qqphp-com/vue-community.git master:master
 
 cd -
